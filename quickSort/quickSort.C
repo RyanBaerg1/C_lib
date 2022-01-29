40e95844e2c171194de731ba7ae2a94ef3b04071
RyanBaerg1 <44005551+RyanBaerg1@users.noreply.github.com>
2022-01-29 11:55:45 -0500

    Updated quick sort with function descriptions

--- quickSort/quickSort.C
+++ quickSort/quickSort.C
@@ -1,5 +1,19 @@
-static void swap(int* arr, int ind1, int ind2);
+/*******************
+Author: Ryan Baerg
+Date: 2022-01-29
+******************/
 
+
+static void swap(int* arr, int ind1, int ind2);
+/***********************************************************************
+Function for swapping 2 indicies in an array using the XOR opperator
+
+Parameters : arr -> pointer to start of the array you want to swap values in
+             ind1 -> index of the first value to swap in arr
+             ind2 -> index of the second value to swap in arr
+             
+Return : Nothing
+************************************************************************/
 static void swap(int* arr, int ind1, int ind2)
 {
   if(ind1 == ind2)
@@ -13,6 +27,20 @@ static void swap(int* arr, int ind1, int ind2)
   arr[ind1] = arr[ind1] ^ arr[ind2];
 }
 
+/***********************************************************************
+Function for performing quick sort algorithm. Algorithm is recursive.
+Quick sort works by choosing a random value in the array and placing all
+values smaller than it on the left and all values larger on the right 
+and then repeating that process until the array is in order.
+
+Parameters : arr -> pointer to start of the array you want to sort
+             low -> index of the current lower bound section of the array
+             you are looking to pivot for.
+             high -> index of the current upper bound section of the array
+             you are looking to pivot for.
+             
+Return : Nothing
+************************************************************************/
 void quickSort(int* arr, int low, int high)
 {
 
